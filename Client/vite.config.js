e6da4f76a5417ff3d1d739108fe33b97d8b29911
Josh Roth <raider4414@gmail.com>
2024-06-05 21:42:38 -0500

    update

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,10 +1,49 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa'; // Import the Vite PWA plugin
 
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({ // Add the Vite PWA plugin
+      manifest: {
+        // Update your manifest options here
+        name: 'Invoicinator',
+        short_name: 'Invoicinator',
+        display: 'standalone',
+        background_color: '#ffffff',
+        lang: 'en',
+        start_url: '/',
+        id: 'https://invoicinator3000-d580657ecca9.herokuapp.com/',
+        description: 'A simple app for invoices',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'https://invoicinator3000-d580657ecca9.herokuapp.com/assets/invoicinator192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'https://invoicinator3000-d580657ecca9.herokuapp.com/assets/invoicinator512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        screenshots: [
+          {
+            src: 'https://invoicinator3000-d580657ecca9.herokuapp.com/assets/longScreenshot.png',
+            sizes: '1080x1920',
+            type: 'image/png'
+          },
+          {
+            src: 'https://invoicinator3000-d580657ecca9.herokuapp.com/assets/screenshot1.png',
+            sizes: '1920x1080',
+            type: 'image/png',
+            form_factor: 'wide'
+          }
+        ]
+      }
+    })
   ],
   server: {
     port: process.env.PORT || 3000,
